d96da65c368da9610ef906f64e0909773c60a0a9
Robin Geral <rgeral@z3r4p3.42lyon.fr>
2021-12-19 17:30:36 +0100

    first commit

---
+++ pipex.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgeral <rgeral@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/15 17:35:25 by rgeral            #+#    #+#             */
+/*   Updated: 2021/12/19 17:07:14 by rgeral           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+#include <fcntl.h>
+
+int main(int	argc, char	*argv[], char	*env[])
+{
+	char	path[] = "/bin/";
+
+	strcat(path, argv[1]);
+	printf("%s\n", path);
+	if (argc > 1)
+		execve(path, argv + 1, env);
+
+}
