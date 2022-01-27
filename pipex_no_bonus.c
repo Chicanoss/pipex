8990223cdbeb341f4e7300511b867e52baeb834f
Robin Geral <rgeral@z4r11p7.42lyon.fr>
2022-01-27 11:01:29 +0100

    1st commit

--- pipex_no_bonus.c
+++ pipex_no_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.c                                            :+:      :+:    :+:   */
+/*   pipex_no_bonus.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rgeral <rgeral@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 17:35:25 by rgeral            #+#    #+#             */
-/*   Updated: 2022/01/18 17:03:50 by rgeral           ###   ########.fr       */
+/*   Updated: 2021/12/25 13:47:45 by rgeral           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ char	**i_shall_path(char	**env)
 	return (NULL);
 }
 
-void	mario_pipe_lover(int *tube,int	*temp_tube, t_args *p, int nb)
+void	mario_pipe_lover(int *tube, t_args *p, int nb)
 {
 	int	file;
 
@@ -53,40 +53,30 @@ void	mario_pipe_lover(int *tube,int	*temp_tube, t_args *p, int nb)
 	{
 		file = open(p->argv[1], 0);
 		close(tube[0]);
-		dup2(temp_tube[1], 1);
+		dup2(tube[1], 1);
 		dup2(file, 0);
-		close(temp_tube[1]);
-		close(file);
-	}
-	else if (nb == p->argc - 2)
-	{
-		file = open(p->argv[p->argc - 1], O_CREAT | O_TRUNC | O_WRONLY);
-		close(tube[1]);
-		dup2(tube[0], 0);
-		dup2(file, 1);
 		close(tube[1]);
 		close(file);
 	}
 	else
 	{
-		dup2(tube[0], 0);
-		dup2(temp_tube[1], 1);
-		close(tube[0]);
+		file = open(p->argv[4], O_CREAT | O_TRUNC | O_WRONLY);
 		close(tube[1]);
-		close(temp_tube[0]);
-		close(temp_tube[1]);
+		dup2(tube[0], 0);
+		dup2(file, 1);
+		close(tube[0]);
+		close(file);
 	}
-
 }
 
-void	*care_child(t_args *p, int nb, int *tube, int	*temp_tube)
+void	*care_child(t_args *p, int nb, int *tube)
 {
 	char	*tmp;
 	int		j;
 	char	**args;
 
 	j = 0;
-	mario_pipe_lover(tube, temp_tube, p, nb);
+	mario_pipe_lover(tube, p, nb);
 	args = ft_split(p->argv[nb], ' ');
 	while (p->path[j])
 	{	
@@ -111,23 +101,13 @@ void	*do_child_not_war(t_args	*p)
 {
 	int		i;
 	int		tube[2];
-	int		temp_tube[2];
 	int		status;
 	pid_t	pid;
 
 	i = 1;
-	while (i++ <= p->argc - 1)
+	pipe(tube);
+	while (i++ < p->argc - 1)
 	{
-		// pipe sauf si dernier tour
-		if (i != p->argc - 1)
-		{
-			pipe(temp_tube);
-		}
-		/*if (i != 2)
-		{
-			pipe(temp_tube);
-		}*/
-		
 		pid = fork();
 		if (pid == -1)
 		{
@@ -135,24 +115,12 @@ void	*do_child_not_war(t_args	*p)
 			return (NULL);
 		}
 		else if (pid == 0)
-			care_child(p, i, tube, temp_tube);
-		if (i > 2)
-		{
-			close(tube[0]);
-			close(tube[1]);
-		}
-		tube[0] = temp_tube[0];
-		tube[1] = temp_tube[1];
-		//tube[0] = temp_tube[0];
-		// fermer pipe du tour precedent sauf si premier tour
-		// mettre les ints de pipe2 dans pipe 1
-		//pipe1[0] = pipe2[0];
-		//[1]
+			care_child(p, i, tube);
 	}
 	close(tube[0]);
 	close(tube[1]);
 	i = -1;
-	while (i++ < p->argc - 3)
+	while (i++ < 2)
 		wait(&status);
 	return (NULL);
 }
@@ -168,8 +136,11 @@ int	main(int argc, char *argv[], char *env[])
 	test.argv = argv;
 	test.env = env;
 	test.argc = argc;
-	
-	printf("Il y a %d args\n", argc);
+	if (argc != 5)
+	{
+		printf("Il y a %d args\n", argc);
+		return (printf("il y a plus que 5 args\n")* 0);
+	}
 	test.path = i_shall_path(env);
 	if (!test.path)
 		return ((int) NULL);
